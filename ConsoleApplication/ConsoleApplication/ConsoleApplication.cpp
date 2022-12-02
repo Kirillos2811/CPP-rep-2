8dc188fa37d8cfdc67ce1ee5cd5750ee0ec41f53
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-03 00:42:06 +0300

    homework 3.5 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,22 +1,12 @@
 ﻿#include <stdio.h>
 
 int main() {
-	long long int pin1, pin2, pass11, pass12, pass21, pass22, pass31, pass32;
-	pass11 = 111;
-	pass12 = 222;
-	pass21 = 333;
-	pass22 = 444;
-	pass31 = 555;
-	pass32 = 666;
-	printf("pin1 = ");
-	scanf_s("%lli", &pin1);
-	printf("pin2 = ");
-	scanf_s("%lli", &pin2);
-	if ((pin1 == pass11 && pin2 == pass12) || (pin1 == pass21 && pin2 == pass22) || (pin1 == pass31 && pin2 == pass32)) {
-		printf("OK");
+	int i = 5;
+	int sum = 0;
+	while (i < 16) {
+		sum = sum + i;
+		i = i + 1;
 	}
-	else {
-		printf("ERROR");
-	}
+	printf("Sum = %i", sum);
 }
 
