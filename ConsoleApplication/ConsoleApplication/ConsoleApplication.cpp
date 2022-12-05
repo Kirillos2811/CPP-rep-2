9d5a605ee35d9f08f090d405824d2711d733ef25
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-05 23:20:06 +0300

    homework 3.6 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,12 +1,45 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int i = 5;
-	int sum = 0;
-	while (i < 16) {
-		sum = sum + i;
-		i = i + 1;
+	int num = 0;
+	while (num != 7) {
+		printf("number? ");
+		scanf_s("%i", &num);
+		if (num > 7) {
+			printf("bolshe 7\n");
+		}else{
+			if (num < 7) {
+				printf("menshe 7\n");
+			}
+			else {
+				printf("ravno 7\n");
+			}
+		}
+		if (num > 10) {
+			printf("bolshe 10\n");
+		}
+		else {
+			if (num < 10) {
+				printf("menshe 10\n");
+			}
+			else {
+				printf("ravno 10\n");
+			}
+		}
+		if (num % 2 == 0) {
+			printf("chetnoe\n");
+		}
+		else {
+			printf("nechetnoe\n");
+		}
+		if (num % 3 == 0) {
+			printf("delitsa na 3\n");
+		}
+		else {
+			printf("ne delitsa na 3\n");
+		}
 	}
-	printf("Sum = %i", sum);
+	printf("SUPER!\n");
+	printf("GAME OVER");
 }
 
