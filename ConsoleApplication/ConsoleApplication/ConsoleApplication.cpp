05ca93ed5d51ec504cf3e09f34af875d843cac05
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-12 23:14:51 +0300

    homework 4.2 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,45 +1,17 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int num = 0;
-	while (num != 7) {
-		printf("number? ");
-		scanf_s("%i", &num);
-		if (num > 7) {
-			printf("bolshe 7\n");
-		}else{
-			if (num < 7) {
-				printf("menshe 7\n");
-			}
-			else {
-				printf("ravno 7\n");
-			}
-		}
-		if (num > 10) {
-			printf("bolshe 10\n");
-		}
-		else {
-			if (num < 10) {
-				printf("menshe 10\n");
-			}
-			else {
-				printf("ravno 10\n");
-			}
-		}
-		if (num % 2 == 0) {
-			printf("chetnoe\n");
-		}
-		else {
-			printf("nechetnoe\n");
-		}
-		if (num % 3 == 0) {
-			printf("delitsa na 3\n");
-		}
-		else {
-			printf("ne delitsa na 3\n");
-		}
+	int n;
+	int arr[1000];
+	printf("Enter the amount of elements: ");
+	scanf_s("%i", &n);
+	printf("Enter the elements:\n");
+	for (int i = 0; i < n; i++) {
+		scanf_s("%i", &arr[i]);
 	}
-	printf("SUPER!\n");
-	printf("GAME OVER");
+	printf("Entered elements:\n");
+	for (int i = 0; i < n; i++) {
+		printf("%2i", arr[i]);
+	}
 }
 
