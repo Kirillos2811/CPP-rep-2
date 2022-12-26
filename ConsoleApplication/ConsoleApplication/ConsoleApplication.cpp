ac29f587e7a042614ab290035cb6f9c86d80a6ce
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-26 22:48:34 +0300

    homework 5.6 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,17 +1,42 @@
 ﻿#include <stdio.h>
 
 int main() {
-	int n;
-	int arr[1000];
-	printf("Enter the amount of elements: ");
+	int arr[100][100];
+	int n, m;
+
+	printf("Enter the amount of rows: ");
 	scanf_s("%i", &n);
+
+	printf("Enter the amount of columns: ");
+	scanf_s("%i", &m);
+
 	printf("Enter the elements:\n");
 	for (int i = 0; i < n; i++) {
-		scanf_s("%i", &arr[i]);
+		for (int j = 0; j < m; j++) {
+			scanf_s("%i", &arr[i][j]);
+		}
 	}
-	printf("Entered elements:\n");
+
+	printf("Entered array:\n");
 	for (int i = 0; i < n; i++) {
-		printf("%2i", arr[i]);
+		for (int j = 0; j < m; j++) {
+			printf("%4i", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (int i = 0; i < n; i++) {
+		int buff = arr[i][0];
+		arr[i][0] = arr[i][m - 1];
+		arr[i][m - 1] = buff;
+	}
+
+	printf("Output array:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%4i", arr[i][j]);
+		}
+		printf("\n");
 	}
 }
 
