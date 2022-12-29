40ca0c184cf6cb96c6ebdb12ad228ddbd17afe6b
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-30 02:31:13 +0300

    homework 6.3 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,42 +1,15 @@
 ﻿#include <stdio.h>
+#include <string>
 
-int main() {
-	int arr[100][100];
-	int n, m;
-
-	printf("Enter the amount of rows: ");
-	scanf_s("%i", &n);
-
-	printf("Enter the amount of columns: ");
-	scanf_s("%i", &m);
-
-	printf("Enter the elements:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf_s("%i", &arr[i][j]);
-		}
-	}
-
-	printf("Entered array:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%4i", arr[i][j]);
-		}
-		printf("\n");
-	}
-
-	for (int i = 0; i < n; i++) {
-		int buff = arr[i][0];
-		arr[i][0] = arr[i][m - 1];
-		arr[i][m - 1] = buff;
-	}
-
-	printf("Output array:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%4i", arr[i][j]);
-		}
-		printf("\n");
-	}
+int main()
+{
+    char str[100];
+    gets_s(str);
+    int length = strlen(str);
+    char buff = str[0];
+    str[0] = str[length - 1];
+    str[length - 1] = buff;
+    printf("Output string:");
+    puts(str);
 }
 
