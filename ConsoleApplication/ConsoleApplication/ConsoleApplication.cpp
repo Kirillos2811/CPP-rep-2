c10e867da624a5fe46189134e843af63741582e6
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-20 18:23:35 +0300

    Homework 8.2 task 2

--- ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication/ConsoleApplication.cpp
@@ -1,135 +1,44 @@
 ﻿#include <stdio.h>
+#include <stdexcept>
+#include <string>
+using namespace std;
 
-class universityAttendee 
-{
-protected:
-    const char* name;
-    int age;
-    bool male;
-    float weight;
-
-public:
-	static const int studentPassType = 1;
-	static const int teacherPassType = 2;
-	static const int scientistPassType = 3;
-	static const int technicianPassType = 4;
-
-    universityAttendee(const char* name, int age, bool male, float weight)
-    {
-        this->name = name;
-        this->age = age;
-        this->male = male;
-        this->weight = weight;
-    }
-
-	const char* getName()
-	{
-		return name;
-	}
-	void setName(const char* name)
-	{
-		this->name = name;
-	}
-
-	int getAge()
-	{
-		return age;
-	}
-	void setAge(int age)
-	{
-		this->age = age;
-	}
-
-	const char* isMale()
-	{
-		if (male == true)
-		{
-			return "true";
-		}
-		else {
-			return "false";
-		}
-	}
-	void setGender(bool male)
-	{
-		this->male = male;
-	}
-
-	float getWeight()
-	{
-		return weight;
-	}
-	void setWeight(float weight)
-	{
-		this->weight = weight;
-	}
-};
-
-class Student : public universityAttendee
+class FileWasNotOpenedException : public runtime_error
 {
 public:
-	const int passType = 1;
-	Student(const char* name, int age, bool male, float weight) : universityAttendee(name, age, male, weight){}
+	FileWasNotOpenedException() : runtime_error("File could not be opened") {}
 };
 
-class Teacher : public universityAttendee
-{
-public: 
-	const int passType = 2;
-	Teacher(const char* name, int age, bool male, float weight) : universityAttendee(name, age, male, weight) {}
-};
+void readFile(string name, char* buff)
+{	
+	FILE* file_ptr;
+	errno_t err;
 
-class Scientist : public universityAttendee
-{
-public:
-	const int passType = 3;
-	Scientist(const char* name, int age, bool male, float weight) : universityAttendee(name, age, male, weight) {}
-};
-
-class Technician : public universityAttendee
-{
-public:
-	const int passType = 4;
-	Technician(const char* name, int age, bool male, float weight) : universityAttendee(name, age, male, weight) {}
-};
-
-void checkPoint(int passType)
-{
-	switch (passType) 
-	{
-	case universityAttendee::studentPassType:
-		printf("Welcome, student. You have been granted access to the student sector\n");
-		break;
-	case universityAttendee::teacherPassType:
-		printf("Welcome, teacher. You have been granted access to the teacher sector\n");
-		break;
-	case universityAttendee::scientistPassType:
-		printf("Welcome, scientist. You have been granted access to the scientist sector\n");
-		break;
-	case universityAttendee::technicianPassType:
-		printf("Welcome, technician. You have been granted access to the technician sector\n");
-		break;
-	default:
-		printf("Undetermined pass type. Access denied\n");
+	err = fopen_s(&file_ptr, name.c_str(), "r");
+	int buff_len = sizeof(buff) / sizeof(buff[0]);
+	
+	if (file_ptr != NULL) {
+		fgets(buff, buff_len, file_ptr);
+		fclose(file_ptr);
+	}
+	else {
+		throw FileWasNotOpenedException();
 	}
 }
 
 int main()
-{	
-	const char* name = "John";
-	Student student = Student(name, 20, true, 75);
-	checkPoint(student.passType);
-
-	name = "Ann";
-	Teacher teacher = Teacher(name, 35, false, 65);
-	checkPoint(teacher.passType);
-
-	name = "Alfred";
-	Scientist scientist = Scientist(name, 60, true, 95);
-	checkPoint(scientist.passType);
-
-	name = "Bob";
-	Technician technician = Technician(name, 45, true, 85);
-	checkPoint(technician.passType);
+{
+	try
+	{	
+		char buff[1000];
+		readFile("nothing.txt", buff);
+		printf("File content:");
+		puts(buff);
+	}
+	catch (const FileWasNotOpenedException& ex)
+	{
+		printf(ex.what());
+	}
+	return 0;
 }
 
